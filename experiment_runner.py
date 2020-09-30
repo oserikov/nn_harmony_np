8121e7481387c7d6aee9a2fcab15b19a02ae66e5
oserikov <srkvoa@gmail.com>
2020-09-30 08:57:04 +0300

    moved experiments logic to the separate module

--- experiment_runner.py
+++ experiment_runner.py
@@ -224,7 +224,7 @@ for epoch in range(epoch_n, MAX_EPOCHS):
 
     # liveloss.update({"train": train_loss, "test": test_loss})
 
-    print(f"epoch: {epoch}", f"train_loss: {train_loss}", f"test_loss: {test_loss}", sep='\t')
+    print(f"lang: {LANG}", f"epoch: {epoch}", f"train_loss: {train_loss}", f"test_loss: {test_loss}", sep='\t')
     logs.append({"train": train_loss, "test": test_loss, "epoch": epoch})
 
     # if epoch % 1 == 0:
